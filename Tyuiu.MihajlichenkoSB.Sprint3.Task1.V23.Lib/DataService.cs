d9b7e097e5d374971fd447ca63c0d29028955155
Сергей Михайличенко <sm4269698@gmail.com>
2025-10-26 23:16:51 +0500

    Ефыл 1

--- Tyuiu.MihajlichenkoSB.Sprint3.Task1.V23.Lib/DataService.cs
+++ Tyuiu.MihajlichenkoSB.Sprint3.Task1.V23.Lib/DataService.cs
@@ -4,22 +4,22 @@ namespace Tyuiu.MihajlichenkoSB.Sprint3.Task1.V23.Lib
 {
     public class DataService : ISprint3Task1V23
     {
-        public double GetMultiplySeries(double x, int startValue, int stopValue)
+
+        public double GetMultiplySeries(int value, int startValue, int stopValue)
         {
             double result = 1.0;
             int k = startValue;
 
-            // Цикл while — как требует условие
             while (k <= stopValue)
             {
-                result *= 300.0 / (Math.Sin(x) + Math.Pow(x, k));
+                result *= 300.0 / (Math.Pow(value, k) - Math.Sin(value)); // изменил знак
                 k++;
             }
 
             return Math.Round(result, 3);
         }
 
-        public double GetMultiplySeries(int value, int startValue, int stopValue)
+        public double GetMultiplySeries(double x, int startValue, int stopValue)
         {
             throw new NotImplementedException();
         }
