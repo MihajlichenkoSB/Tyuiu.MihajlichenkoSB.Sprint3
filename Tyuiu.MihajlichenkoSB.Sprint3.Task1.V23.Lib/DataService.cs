910390ea1dbca953a9da236fd5a86bbf685d0f56
Сергей Михайличенко <sm4269698@gmail.com>
2025-10-27 00:12:13 +0500

    Task 1

--- Tyuiu.MihajlichenkoSB.Sprint3.Task1.V23.Lib/DataService.cs
+++ Tyuiu.MihajlichenkoSB.Sprint3.Task1.V23.Lib/DataService.cs
@@ -4,22 +4,22 @@ namespace Tyuiu.MihajlichenkoSB.Sprint3.Task1.V23.Lib
 {
     public class DataService : ISprint3Task1V23
     {
-        public double GetMultiplySeries(int value, int startValue, int stopValue)
-        {
-            double product = 1.0;
-            int k = startValue;
+public double GetMultiplySeries(int value, int startValue, int stopValue)
+{
+    double product = 1.0;
+    int k = startValue;
 
-            while (k <= stopValue)
-            {
-                double denominator = Math.Sin(value) + Math.Pow(value, k);
-                double fraction = -300.0 / denominator;
-                double term = Math.Pow(fraction, k);
-                product *= term;
+    while (k <= stopValue)
+    {
+        double denominator = Math.Sin(value) + Math.Pow(value, k);
+        double fraction = 300.0 / denominator; // исправлено
+        double term = Math.Pow(fraction, k);
+        product *= term;
 
-                k++;
-            }
+        k++;
+    }
 
-            return product;
+             return product;
         }
     }
 }
