c14ccc79128021d2aa00083482feb66de6b0246c
Сергей Михайличенко <sm4269698@gmail.com>
2025-10-26 23:19:12 +0500

    Ефыл 1

--- Tyuiu.MihajlichenkoSB.Sprint3.Task1.V23.Lib/DataService.cs
+++ Tyuiu.MihajlichenkoSB.Sprint3.Task1.V23.Lib/DataService.cs
@@ -7,12 +7,13 @@ namespace Tyuiu.MihajlichenkoSB.Sprint3.Task1.V23.Lib
 
         public double GetMultiplySeries(int value, int startValue, int stopValue)
         {
+            double x = value; // конвертируем int в double
             double result = 1.0;
             int k = startValue;
 
             while (k <= stopValue)
             {
-                result *= 300.0 / (Math.Pow(value, k) - Math.Sin(value)); // изменил знак
+                result *= 300.0 / (Math.Pow(x, k) - Math.Sin(x)); // используем правильную формулу
                 k++;
             }
 
