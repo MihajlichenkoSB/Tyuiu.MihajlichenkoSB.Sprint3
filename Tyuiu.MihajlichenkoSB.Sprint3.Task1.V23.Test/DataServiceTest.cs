811a73b97dc495ff47b99d005f683c0d7d6ee77f
Сергей Михайличенко <sm4269698@gmail.com>
2025-10-27 00:10:09 +0500

    Task 1

--- Tyuiu.MihajlichenkoSB.Sprint3.Task1.V23.Test/DataServiceTest.cs
+++ Tyuiu.MihajlichenkoSB.Sprint3.Task1.V23.Test/DataServiceTest.cs
@@ -10,16 +10,16 @@ namespace Tyuiu.MihajlichenkoSB.Sprint3.Task1.V23.Test
         {
 
             DataService ds = new DataService();
-            int value = 5;          
-            int startValue = 1;      
-            int stopValue = 5;       
+            int value = 5;
+            int startValue = 1;
+            int stopValue = 5;
 
-            double expected = -0.071331455025152; 
+            double expected = -0.071331455025152;
 
             double actual = ds.GetMultiplySeries(value, startValue, stopValue);
 
             double delta = 1e-12;
-            Assert.AreEqual(expected, actual, delta, $"Ожидали примерно {expected}, получили {actual}");
+            Assert.AreEqual(expected, actual, delta);
         }
     }
 }
