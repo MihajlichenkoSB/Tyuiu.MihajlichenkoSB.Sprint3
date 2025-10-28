d59039849b5d9818d0bc56c93b5e663329c7ccd8
Сергей Михайличенко <sm4269698@gmail.com>
2025-10-28 19:02:12 +0500

    Task 6

---
+++ Tyuiu.MihajlichenkoSB.Sprint3.Task6.V3.Test/DataServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Numerics;
+using Tyuiu.MihajlichenkoSB.Sprint3.Task6.V3.Lib;
+
+namespace Tyuiu.MihajlichenkoSB.Sprint3.Task6.V3.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest 
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int startValue = 13;
+            int endValue = 19;
+
+            int res = ds.GetSumTheDivisors(startValue, endValue);
+
+            int wait = 120;
+
+            Assert.AreEqual(wait, res); 
+        }
+    }
+}
+
---
+++ Tyuiu.MihajlichenkoSB.Sprint3.Task6.V3/Program.cs
@@ -0,0 +1,38 @@
+﻿using Tyuiu.MihajlichenkoSB.Sprint3.Task6.V3.Lib;
+
+namespace Tyuiu.MihajlichenkoSB.Sprint3.Task6.V3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 |  Выполнил: Михайличенко С.Б. | ИИПб-25-1";
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* Спринт #3                                                                *");
+            Console.WriteLine("* Тема: Использование операторов Continue и break в циклах                 *");
+            Console.WriteLine("* Задание #6                                                               *");
+            Console.WriteLine("* Вариант #3                                                               *");
+            Console.WriteLine("* Выполнил: Михайличенко Сергей Борисович | ИИПб-25-1                      *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                 *");
+            Console.WriteLine("* Напишите программу, которая ищет среди целых чисел, принадлежащих        *");
+            Console.WriteLine("* числовому отрезку[13, 19] сумму всех делителей больше 8                  *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+            Console.WriteLine("****************************************************************************");
+
+            int startValue = 13;
+            int stopValue = 19;
+
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                               *");
+            Console.WriteLine("****************************************************************************");
+
+            Console.WriteLine("Сумма делится = " + ds.GetSumTheDivisors(startValue, stopValue));
+
+            Console.ReadKey();
+        }
+    }
+}
+            
